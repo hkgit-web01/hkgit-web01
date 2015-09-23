7cddd759cf2799a80bfaa6f98366c5770f1389d6
HK Yoon <hkzuuu@gmail.com>
2015-09-24 00:11:38 +0900

    modifications according to YN 48

--- js/script.js
+++ js/script.js
@@ -1,7 +1,49 @@
-
+var Gallery;
 window.onload = function(){
 	var gallery = document.querySelectorAll(".gallery");
-	gallery[0].classList.add("loaded");
+	if(gallery.length){ 
+		gallery[0].classList.add("loaded");
+
+		Gallery = (function(){
+
+			var screenDom = document.getElementById("screen");
+			var galleryImages = document.querySelectorAll(".gallery img.thumb");
+			var i;
+
+			function refreshThumb() {
+				var i;
+				for (i = 0; i < galleryImages.length; i++){
+					galleryImages[i].classList.remove("selected");
+				}	
+			}
+
+			function screenUpdate(src) {
+				refreshThumb();
+				screenDom.style.opacity="0";
+				screenDom.onload = function(){
+					screenDom.style.opacity="1";
+					console.log("loaded");
+				};
+				screenDom.src = src;
+			}
+
+			for (i = 0; i < galleryImages.length; i++){
+				galleryImages[i].onclick = function(){
+					screenUpdate(this.src);
+					this.classList.add("selected");
+				};
+			}
+
+			return {
+				update: screenUpdate	
+
+
+
+			};
+		})();
+
+	}
+	
 
 	var menuOpenDom = document.querySelectorAll(".menuOpen");
 	menuOpenDom[0].onclick = function(){
@@ -15,43 +57,7 @@ window.onload = function(){
 
 };
 
-var Gallery = (function(){
 
-	var screenDom = document.getElementById("screen");
-	var galleryImages = document.querySelectorAll(".gallery img.thumb");
-	var i;
-
-	function refreshThumb() {
-		var i;
-		for (i = 0; i < galleryImages.length; i++){
-			galleryImages[i].classList.remove("selected");
-		}	
-	}
-
-	function screenUpdate(src) {
-		refreshThumb();
-		screenDom.style.opacity="0";
-		screenDom.onload = function(){
-			screenDom.style.opacity="1";
-			console.log("loaded");
-		};
-		screenDom.src = src;
-	}
-
-	for (i = 0; i < galleryImages.length; i++){
-		galleryImages[i].onclick = function(){
-			screenUpdate(this.src);
-			this.classList.add("selected");
-		};
-	}
-
-	return {
-		update: screenUpdate	
-
-
-
-	};
-})();
 
 
 
