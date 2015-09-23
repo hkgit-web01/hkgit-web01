654e43df5a6a32c4f7c2aca48f071ed755617413
HK Yoon <hkzuuu@gmail.com>
2015-09-23 19:13:00 +0900

    modifications according to YN 44

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
-// document.addEventListener("DOMContentLoaded", function(event){
-// 	// alert("loaded");
-// });
+
+window.onload = function(){
+	var gallery = document.querySelectorAll(".gallery");
+	gallery[0].classList.add("loaded");
+
+
+};
+
+var Gallery = (function(){
+
+	var screenDom = document.getElementById("screen");
+	var galleryImages = document.querySelectorAll(".gallery img");
+	var i;
+
+	function refreshThumb() {
+		var i;
+		for (i = 0; i < galleryImages.length; i++){
+			galleryImages[i].classList.remove("selected");
+		}	
+	}
+
+	function screenUpdate(src) {
+		refreshThumb();
+		screenDom.style.opacity="0";
+		screenDom.onload = function(){
+			screenDom.style.opacity="1";
+			console.log("loaded");
+		};
+		screenDom.src = src;
+	}
+
+	for (i = 0; i < galleryImages.length; i++){
+		galleryImages[i].onclick = function(){
+			screenUpdate(this.src);
+			this.classList.add("selected");
+		};
+	}
+
+	return {
+		update: screenUpdate	
+
+
+
+	};
+})();
+
+
+
+
